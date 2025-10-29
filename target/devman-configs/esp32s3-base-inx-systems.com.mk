4405b2593da1515bc538c0805514c8ee66c28cde
inx-iot <developer@inx-systems.com>
2025-10-29 01:23:12 +0000

    Added missing devman config file for the esp32 targets.

---
+++ target/devman-configs/esp32s3-base-inx-systems.com.mk
@@ -0,0 +1,8 @@
+
+DEVMAN_SERVER_DOMAIN=www.inx-systems.com
+DEVMAN_SERVER_PROTOCOL=mqtts
+
+# Server config & credentials for uplading OTA updates
+EHS_PRODUCT_NAME=esp32s3-base
+DEVMAN_UNAME=_NOT_SAFE_USERNAME_
+SSHPORT=22
