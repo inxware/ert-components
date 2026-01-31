89b0a96cded9d1416a48d6962e2a26448d3cb9bd
inx-iot <developer@inx-systems.com>
2026-01-31 16:22:39 +0000

    Changed default regression test to include more targets.

--- SystemTests/CI/regression_test-published-only.sh
+++ SystemTests/CI/regression_test-published-only.sh
@@ -25,20 +25,8 @@ TXT_FG=${TXT_FG_CYAN}
 #esp32s3_freertos-xtensa-base-debug       linux_android_arm64              linux_arm_gtk_gst_raspberrypi        linux_x86_64_clang_gg_debian10     linux_x86_64_gtk_gst_debian11                   nxp_arm                 win_x86_lvgl
 #linux_x86_64_clang_g
 
-target_platform=(
-'esp32_freertos-xtensor-base'
-'esp32s3_freertos-xtensa-base'
-'esp32s3_freertos-xtensa-base-debug'
-'esp32s3_freertos-xtensa-base_n8r2'
-'esp32s3_freertos-xtensa-community'
-'esp32s3_freertos-xtensa-demo-loradisp'
-'esp32s3_freertos-xtensa-n8r2_waveshare_wearable'
-'esp32s3_freertos-xtensa-no-certs'
-'esp32s3_freertos-xtensa-waveshare-touchlcd-1_28'
-'esp32s3_freertos-xtensa-waveshare-touchlcd-1_69'
-)
 
-target_platforXm=(
+target_platforX_small=(
     'linux_x86_64_clang_gtk_gst_gg_debian11-no-certs'
     'linux_x86_64_clang_lvgl_debian11-no-certs'
     'linux_arm64_gtk_gst_gg_debian11'
@@ -49,12 +37,12 @@ target_platforXm=(
     'win_x86_32-lucid-win10'
 )
 
-target_platform_medium=(
-    'linux_amd64'
-    'linux_amd64_gtk_gst'
+target_platform=(
+    'linux_amd64_debian9'
+    'linux_amd64_gtk_gst_debian9'
     'linux_x86_64_clang'
     'linux_x86_64_clang_gtk'
-    'linux_x86_64_clang_gtk_gst_gg_debian11'
+    'linux_x86_64_clang_gtk_gst_gg_debian11-no-certs'
     'linux_x86_64_gtk_gst_debian11'
     'linux_x86_64_clang_lvgl_debian11'
     'linux_armv7l_clang_debian10'
@@ -62,11 +50,13 @@ target_platform_medium=(
     'linux_arm64_lvgl_gg_debian10'
     'linux_arm64_lvgl_gg_debian11'
     'linux_arm64_gtk_gst_gg_debian11'
+    'linux_android_arm'
     'esp32_freertos-xtensor-base'
     'esp32s3_freertos-xtensa-base'
+    'arduino_arduino-mbed-nano-image_community'
+    'arduino_arduino-mbed-nano-lib_community'
     'win_x86_gtk_gst'
     'win_x86_32-lucid-win10'
-    'linux_android_arm'
 )
 
 echo "Testing the following platforms:"
