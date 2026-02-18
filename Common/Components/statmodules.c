dd05eba2ad9c38077bad468df426748a72636940
Xiaosheng An <x.an@inx-systems.com>
2026-02-18 14:15:16 +0000

    fix: implicit function declarations and parameter types
    Signed-off-by: Xiaosheng An <x.an@inx-systems.com>

--- Common/Components/statmodules.c
+++ Common/Components/statmodules.c
@@ -35,7 +35,7 @@ extern const EhsBlockRefType EhsBlockRefTable_core[];
 #ifdef EHS_GUI_SUPPORT
 #include "hal_viewport.h"
 #include "widget.h"
-extern const   EhsBlockRefTable_coreGui[];
+extern const EhsBlockRefType EhsBlockRefTable_coreGui[];
 #endif
 
 /**
