67f65a9cf93c4a387c130b922d88f44e7b7f554d
inx-iot <developer@inx-systems.com>
2026-03-18 01:51:16 +0000

    typo in sfera make file fix

--- target/Component-HAL/uart/sferalabs/target_uart.mk
+++ target/Component-HAL/uart/sferalabs/target_uart.mk
@@ -2,4 +2,4 @@
 # VPATH, INC_DIRS for this directory, and OBJECTS are set by uart_common.mk.
 INC_DIRS += $(EHS_TARGET_COMPONENT_HAL_PATH)/sferalabs
 # Background receive thread requires pthreads.
-// Always anyway for linuix :LIBS += -lpthread
+# Always anyway for linuix :LIBS += -lpthread
